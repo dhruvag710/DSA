61c7400a16354dc742e837b77b28ec26d032233b
Dhruv Agarwal <169169013+dhruvag710@users.noreply.github.com>
2025-10-01 21:53:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (27.49%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,42 +1,51 @@
 class MyQueue {
-
     private:
     stack<int>s1,s2;
-
 public:
     MyQueue() {
         
     }
     
     void push(int x) {
-        while(s1.size())
-        {
-            s2.push(s1.top());
-            s1.pop();
-        }
         s1.push(x);
-        while(s2.size())
-        {
-            s1.push(s2.top());
-            s2.pop();
-        }
-        
     }
     
     int pop() {
-        if(s1.empty()) return -1;
-        int val=s1.top();
-        s1.pop();
-        return val;
+        if(!s2.empty())
+        {
+            int val=s2.top();
+            s2.pop();
+            return val;
+        }
+        else
+        {
+            while(!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+            int vall=s2.top();
+            s2.pop();
+            return vall;
+        }
     }
     
     int peek() {
-        if(s1.empty()) return -1;
-        else return s1.top();
+        if(!s2.empty()) return s2.top();
+        else
+        {
+            while(!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+           return s2.top();
+            
+        }
     }
     
     bool empty() {
-        return s1.empty();
+         return s1.empty() && s2.empty();
     }
 };
 

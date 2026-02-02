ec009625fce294870db4421ebff754e7a678b306
Dhruv Agarwal <169169013+dhruvag710@users.noreply.github.com>
2026-02-02 20:09:21 +0530

    Time: 571 ms (6.79%) | Memory: 73.5 MB (54.04%) - LeetSync

--- 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -1,70 +1,76 @@
+static const auto fast_io = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+
 class Solution {
 public:
     int minimumDifference(vector<int>& nums) {
-        int n = nums.size();
-        int mid = n / 2;
+        int n=nums.size();
+        int mid=n/2;
+        vector<int> left(nums.begin(), nums.begin()+mid);
+        vector<int> right(nums.begin()+mid, nums.end());
+        long long sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+        }
+        
+        vector<vector<int>> leftSums(mid+1), rightSums(mid+1);
 
-        vector<int> left(nums.begin(), nums.begin() + mid);
-        vector<int> right(nums.begin() + mid, nums.end());
-
-        long long totalSum = 0;
-        for (int x : nums) totalSum += x;
-
-        vector<vector<int>> leftSums(mid + 1), rightSums(mid + 1);
-
-        for (int i = 0; i <= mid; i++) {
-            leftSums[i].reserve(1 << mid);
-            rightSums[i].reserve(1 << mid);
+        for(int mask=0; mask<(1<<mid); mask++)
+        {
+            long long ans=0;
+             int count=0;
+            for(int i=0;i<mid;i++)
+            {
+                if(mask & (1<<i)) ans+=left[i], count++;
+                
+            }
+            leftSums[count].push_back(ans);
         }
 
-        for (int mask = 0; mask < (1 << mid); mask++) {
-            int s = 0, cnt = 0;
-            for (int i = 0; i < mid; i++) {
-                if (mask & (1 << i)) {
-                    s += left[i];
-                    cnt++;
-                }
+        for(int mask=0; mask<(1<<mid); mask++)
+        {
+            long long ans=0;
+             int count=0;
+            for(int i=0;i<mid;i++)
+            {
+                if(mask & (1<<i)) ans+=right[i],count++;
+                
             }
-            leftSums[cnt].push_back(s);
-        }
-
-        for (int mask = 0; mask < (1 << mid); mask++) {
-            int s = 0, cnt = 0;
-            for (int i = 0; i < mid; i++) {
-                if (mask & (1 << i)) {
-                    s += right[i];
-                    cnt++;
-                }
-            }
-            rightSums[cnt].push_back(s);
+            rightSums[count].push_back(ans);
         }
 
         for (int i = 0; i <= mid; i++) {
-            sort(leftSums[i].begin(), leftSums[i].end());
-            sort(rightSums[i].begin(), rightSums[i].end());
-        }
+    sort(leftSums[i].begin(), leftSums[i].end());
+    sort(rightSums[i].begin(), rightSums[i].end());
+}
 
-        long long ans = LLONG_MAX;
 
-        for (int k = 0; k <= mid; k++) {
-            auto &rs = rightSums[mid - k];
-            for (int ls : leftSums[k]) {
-                int target = (int)(totalSum / 2 - ls);
+        long long ans=INT_MAX;
 
-                auto it = lower_bound(rs.begin(), rs.end(), target);
+        for(int k=0;k<=mid;k++)
+        {
+            for(int ls: leftSums[k])
+            {
+                int reqd=(sum/2)-ls;
 
-                if (it != rs.end())
-                    ans = min(ans, llabs(totalSum - 2LL * (ls + *it)));
-
-                if (it != rs.begin()) {
-                    --it;
-                    ans = min(ans, llabs(totalSum - 2LL * (ls + *it)));
+                auto it=lower_bound(rightSums[mid-k].begin(), rightSums[mid-k].end(), reqd);
+                if(it!=rightSums[mid-k].end())
+                {
+                    ans=min(ans, llabs(sum- 2*(*it+ls)));
                 }
 
-                if (ans == 0) return 0; 
+                if(it!=rightSums[mid-k].begin())
+                {
+                    it--;
+                    ans=min(ans, llabs(sum- 2*(*it+ls)));
+                }
             }
-        }
-
-        return (int)ans;
+        }       
+        return ans;
     }
 };

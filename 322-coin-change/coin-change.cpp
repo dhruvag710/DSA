ca185a37df2a2b531a43f56879e2c5e765c785b5
Dhruv Agarwal <169169013+dhruvag710@users.noreply.github.com>
2026-02-17 14:42:17 +0530

    Time: 47 ms (34.49%) | Memory: 48.7 MB (43.46%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    int solve(int i, vector<int>& coins, int amount, vector<vector<int>> &dp) {
-    if (i == 0) {
-        if (amount % coins[0] == 0)
-            return amount / coins[0];
-        return 1e9;
-    }
-    if(dp[i][amount]!=-1) return dp[i][amount];
-    int notTake = solve(i - 1, coins, amount,dp);
-
-    int take = 1e9;
-    if (coins[i] <= amount) {
-        take = 1 + solve(i, coins, amount - coins[i],dp);
-    }
-
-    return dp[i][amount]=min(take, notTake);
-}
 
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans = solve(n - 1, coins, amount,dp);
-        return (ans >= 1e9) ? -1 : ans;
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        for(int t=0; t<=amount; t++)
+        {
+            if(t % coins[0]==0) dp[0][t]=t/coins[0];
+            else dp[0][t]=1e9;
+        }
+        for(int ind=1;ind<n;ind++){
+            for(int t=0;t<=amount;t++)
+            {
+                int notTake=0+dp[ind-1][t];
+                int take=INT_MAX;
+                if(coins[ind]<=t){
+                    take=1+dp[ind][t-coins[ind]];
+                }
+                dp[ind][t]=min(take, notTake);
+            }
+        }
 
+        int ans=dp[n-1][amount];
+        if(ans>=1e9) return -1;
+        return ans;
     }
 };

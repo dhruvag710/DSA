1adeea58b00b3c56b8fd09d44eab30688db32e80
Dhruv Agarwal <169169013+dhruvag710@users.noreply.github.com>
2026-03-11 00:52:19 +0530

    Time: 1 ms (74.59%) | Memory: 10.4 MB (99.79%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,26 +1,27 @@
 class Solution {
-public:
-    int characterReplacement(string s, int k) {
-        int n=s.length();
-        int ans=0, left=0, right=0, maxFreq=0;
-        map<char,int>mpp;
-
-        for(int right=0;right<n;right++)
+  public:
+    int characterReplacement(string& s, int k) {
+        vector<int> freq(26,0);
+        
+        int left = 0;
+        int maxFreq = 0;
+        int ans = 0;
+        
+        for(int right = 0; right < s.length(); right++)
         {
-            mpp[s[right]]++;
-            maxFreq=max(maxFreq, mpp[s[right]]);
-
-            while(right-left+1-maxFreq >k)
+            freq[s[right] - 'A']++;
+            
+            maxFreq = max(maxFreq, freq[s[right] - 'A']);
+            
+            while((right - left + 1) - maxFreq > k)
             {
-                mpp[s[left]]--;
-                if(mpp[s[left]]==0) mpp.erase(s[left]);
-
+                freq[s[left] - 'A']--;
                 left++;
             }
-            ans=max(ans, right-left+1);
+            
+            ans = max(ans, right - left + 1);
         }
+        
         return ans;
-
-
     }
 };

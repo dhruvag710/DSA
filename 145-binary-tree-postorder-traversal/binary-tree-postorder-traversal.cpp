040f6ce6e54cf5d52d64c89065ec3239e88fb195
Dhruv Agarwal <169169013+dhruvag710@users.noreply.github.com>
2026-03-15 21:02:59 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (44.83%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -1,32 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void postOrder(TreeNode* root, vector<int>& ans)
+    vector<int> postorderTraversal(TreeNode* root)
     {
-        if(root==nullptr) return;
-
-        postOrder(root->left, ans);
-        postOrder(root->right, ans);
-        ans.push_back(root->val);
-
-
-    }
-
-
-
-    vector<int> postorderTraversal(TreeNode* root) {
         vector<int>ans;
-        postOrder(root, ans);
+        if(root==nullptr) return ans;
+
+        stack<TreeNode*>st1, st2;
+        st1.push(root);
+
+        while(!st1.empty())
+        {
+            root=st1.top();
+
+            st1.pop();
+            st2.push(root);
+            if(root->left!=NULL)
+            {
+                st1.push(root->left);
+            }
+            if(root->right!=NULL)
+            {
+                st1.push(root->right);
+            }
+        }
+        while(!st2.empty())
+        {
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
         return ans;
+
+
     }
+
 };

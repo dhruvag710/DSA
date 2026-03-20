1056a05907ffb23a56f38e532150e738ae03243e
Dhruv Agarwal <169169013+dhruvag710@users.noreply.github.com>
2026-03-20 12:53:10 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (17.53%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,41 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
+        vector<vector<int>>ans;
         if(root==NULL) return ans;
         queue<TreeNode*> q;
         q.push(root);
+
         bool leftToRight=true;
 
-        while(!q.empty())
-        {
+        while(!q.empty()){
             int n=q.size();
-            vector<int>anss(n);
+            vector<int>level;
 
             for(int i=0;i<n;i++)
             {
-                TreeNode* temp=q.front();
+                TreeNode* node=q.front();
                 q.pop();
-                int index=(leftToRight) ? i : (n-1-i);
-
-                anss[index]=temp->val;
-                if(temp->left!=nullptr) q.push(temp->left);
-                if(temp->right!=nullptr) q.push(temp->right);
+                level.push_back(node->val);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
             }
+            if(!leftToRight)
+            {
+                reverse(level.begin(), level.end());
+            }
+            ans.push_back(level);
             leftToRight=!leftToRight;
-            ans.push_back(anss);
         }
-        return ans;
+        return ans; 
     }
 };

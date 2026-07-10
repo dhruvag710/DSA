2f2a91f85fbc3e7cca554ab97cedb0488f480a51
Dhruv Agarwal <169169013+dhruvag710@users.noreply.github.com>
2026-07-10 15:49:26 +0530

    Time: 20 ms (86.93%) | Memory: 69.3 MB (54.92%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -1,48 +1,36 @@
 class Solution {
 public:
-    bool detectCycle(int sr, int sc, vector<vector<char>>&grid, vector<vector<int>>& vis)
+    bool dfs(int sr, int sc, int pr, int pc, vector<vector<char>>& grid, vector<vector<int>>& vis)
     {
         int n=grid.size();
         int m=grid[0].size();
+
         int row[]={0,1,0,-1};
         int col[]={1,0,-1,0};
 
-        queue<pair<pair<int,int>,pair<int,int>>>q;
-        q.push({{sr,sc},{-1,-1}});
         vis[sr][sc]=1;
-        while(!q.empty())
-        {
-            int sr=q.front().first.first;
-            int sc=q.front().first.second;
-            int pr=q.front().second.first;
-            int pc=q.front().second.second;
-            q.pop();
 
-            for(int i=0;i<4;i++)
+        for(int i=0;i<4;i++)
+        {
+            int nr=sr+row[i];
+            int nc=sc+col[i];
+
+            if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==grid[sr][sc])
             {
-                int nr=sr+row[i];
-                int nc=sc+col[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==grid[sr][sc])
+                if(!vis[nr][nc])
                 {
-                    if(!vis[nr][nc])
-                    {
-                        vis[nr][nc]=1;
-                        q.push({{nr,nc},{sr,sc}});
-                    }
-                    else if(!(nr==pr && nc==pc)) return true;
+                    if(dfs(nr,nc,sr,sc,grid,vis)) return true;
                 }
+                else if(!(nr==pr && nc==pc)) return true;
             }
         }
         return false;
-
     }
-
-
-
 
     bool containsCycle(vector<vector<char>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
+
         vector<vector<int>>vis(n,vector<int>(m,0));
 
         for(int i=0;i<n;i++)
@@ -51,7 +39,7 @@ public:
             {
                 if(!vis[i][j])
                 {
-                    if(detectCycle(i,j,grid,vis)) return true;
+                    if(dfs(i,j,-1,-1,grid,vis)) return true;
                 }
             }
         }

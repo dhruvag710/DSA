384b0191f0677353f19798f1962b36f0cfc0f166
Dhruv Agarwal <169169013+dhruvag710@users.noreply.github.com>
2026-07-10 14:52:04 +0530

    Time: 152 ms (21.59%) | Memory: 214.9 MB (8.84%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -1,8 +1,6 @@
-//bfs solution
-
 class Solution {
 public:
-    bool detectCycle(int sr, int sc, vector<vector<char>>& grid, vector<vector<int>>&vis)
+    bool detectCycle(int sr, int sc, vector<vector<char>>&grid, vector<vector<int>>& vis)
     {
         int n=grid.size();
         int m=grid[0].size();
@@ -12,35 +10,32 @@ public:
         queue<pair<pair<int,int>,pair<int,int>>>q;
         q.push({{sr,sc},{-1,-1}});
         vis[sr][sc]=1;
-
         while(!q.empty())
         {
-            int r=q.front().first.first;
-            int c=q.front().first.second;
+            int sr=q.front().first.first;
+            int sc=q.front().first.second;
             int pr=q.front().second.first;
             int pc=q.front().second.second;
-
             q.pop();
 
             for(int i=0;i<4;i++)
             {
-                int nr=r+row[i];
-                int nc=c+col[i];
-
-                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==grid[r][c])
+                int nr=sr+row[i];
+                int nc=sc+col[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==grid[sr][sc])
                 {
                     if(!vis[nr][nc])
                     {
                         vis[nr][nc]=1;
-                        q.push({{nr,nc},{r,c}});
+                        q.push({{nr,nc},{sr,sc}});
                     }
                     else if(!(nr==pr && nc==pc)) return true;
                 }
             }
         }
         return false;
-    }
 
+    }
 
 
 
@@ -48,8 +43,8 @@ public:
     bool containsCycle(vector<vector<char>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-
         vector<vector<int>>vis(n,vector<int>(m,0));
+
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
